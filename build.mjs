3441328b412b8631df310c0ff0d1e22d4492f71f
Rie Takahashi <megumin@megu.dev>
2025-07-17 22:44:25 +0100

    fix: update esbuild-config

--- build.mjs
+++ build.mjs
@@ -8,22 +8,22 @@ const watch = process.argv.includes("--watch");
 const clean = process.argv.includes("--clean");
 
 if (clean) {
-  fs.rmSync("./dist", { recursive: true, force: true });
+	fs.rmSync("./dist", { recursive: true, force: true });
 } else {
-  const exts = fs.readdirSync("./src");
+	const exts = fs.readdirSync("./src");
 
-  for (const ext of exts) {
-    const cfg = {
-      src: path.resolve(path.join("src", ext)),
-      dst: path.resolve(path.join("dist", ext)),
-      esm: esm.includes(ext),
-      ext
-    };
+	for (const ext of exts) {
+		const cfg = {
+			src: path.resolve(path.join("src", ext)),
+			dst: path.resolve(path.join("dist", ext)),
+			esm: esm.includes(ext),
+			ext,
+		};
 
-    if (watch) {
-      await watchExt(cfg);
-    } else {
-      await buildExt(cfg);
-    }
-  }
+		if (watch) {
+			await watchExt(cfg);
+		} else {
+			await buildExt(cfg);
+		}
+	}
 }
