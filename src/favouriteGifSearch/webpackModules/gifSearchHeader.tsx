469be638bbf522b6d2fe7f599c01afea90bf8ea2
megumin <megumin@megu.dev>
2025-09-09 20:54:16 +0100

    Update gifSearchHeader.tsx

--- src/favouriteGifSearch/webpackModules/gifSearchHeader.tsx
+++ src/favouriteGifSearch/webpackModules/gifSearchHeader.tsx
@@ -63,7 +63,7 @@ export function renderGifSearchHeader(_this: GifPickerThis) {
 
   return (
     <SearchBar
-      className={styles.searchBar}
+      className={styles.input}
       size="md"
       query={query}
       onChange={onChange}
