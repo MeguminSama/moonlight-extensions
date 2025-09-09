272bd659faac66f2f24d00fd6f932d9582cb0e8f
megumin <megumin@megu.dev>
2025-09-09 20:52:43 +0100

    Update gifSearchHeader.tsx

--- src/favouriteGifSearch/webpackModules/gifSearchHeader.tsx
+++ src/favouriteGifSearch/webpackModules/gifSearchHeader.tsx
@@ -6,7 +6,7 @@ import Fuse from "fuse.js";
 
 const i18n = spacepack.require("discord/intl");
 const SearchBar = spacepack.require("discord/uikit/search/SearchBar").default;
-const styles = spacepack.findByCode("searchBar:", "searchHeader:", "header:")[0].exports;
+const styles = spacepack.findByCode("input:", "container:", "iconButton:")[0].exports;
 
 interface FavouriteGif {
   order: number;
